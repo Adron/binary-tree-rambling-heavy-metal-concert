aa0bff436cab7aa04d8f70a81d79477c2cf52365
Adron Hall <adron@thrashingcode.com>
2024-05-13 23:25:49 -0700

    Final refactoring.

--- Tree/Climber.cs
+++ Tree/Climber.cs
@@ -4,17 +4,13 @@ public static class Climber
 {
     public static int MaxSum(TreeNode root)
     {
-        return root == null ? 0 : FindMaxSum(root);
-    }
-
-    private static int FindMaxSum(TreeNode? node)
-    {
-        if (node == null) return 0;
-        if (node.left == null && node.right == null) return node.value;
-
-        int leftMaxSum = node.left != null ? FindMaxSum(node.left) : int.MinValue;
-        int rightMaxSum = node.right != null ? FindMaxSum(node.right) : int.MinValue;
-
-        return node.value + Math.Max(leftMaxSum, rightMaxSum);
+        return root switch
+        {
+            null => 0,
+            {left: null,  right: null,  value: var v} => v,
+            {left: null,  right: var r, value: var v} => v + MaxSum(r),
+            {left: var l, right: null,  value: var v} => v + MaxSum(l),
+            {left: var l, right: var r, value: var v} => v + System.Math.Max(MaxSum(l), MaxSum(r)),
+        };
     }
 }
